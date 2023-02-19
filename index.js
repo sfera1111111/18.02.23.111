0a41e6754d39e93fb2d1d688baee9b959034f043
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-02-19 20:55:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//1. Напишите функцию, которая выводит ваше имя и фамилию.
+// functionName("Erjan", "Kydyrov") // My  name is Erjan Kydyrov
+
+function myName(a, b) {
+  console.log(a + b);
+}
+myName("Erjan ", "Kydyrov");
